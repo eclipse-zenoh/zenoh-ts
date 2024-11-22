07303abe6bb4ac0878d31117aae5e0469b943fb5
Charles Schleich <schleich.cdaniel@gmail.com>
2024-11-22 14:16:56 +0100

    Add bumping of internal deps for plugin (#44)
    * Add Bump Deps
    
    * remove Branch, add bump dep inputs for workflow
    
    * update package.metadata.deb.depends

--- ci/scripts/bump-and-tag.bash
+++ ci/scripts/bump-and-tag.bash
@@ -5,6 +5,12 @@ set -xeo pipefail
 readonly live_run=${LIVE_RUN:-false}
 # Release number
 readonly version=${VERSION:?input VERSION is required}
+# Dependencies' pattern
+readonly bump_deps_pattern=${BUMP_DEPS_PATTERN:-''}
+# Dependencies' version
+readonly bump_deps_version=${BUMP_DEPS_VERSION:-''}
+# Dependencies' git branch
+readonly bump_deps_branch=${BUMP_DEPS_BRANCH:-''}
 # Git actor name
 readonly git_user_name=${GIT_USER_NAME:?input GIT_USER_NAME is required}
 # Git actor email
@@ -37,6 +43,33 @@ cat ${package_json_path} | jq "$JQ"  > "$package_tmp"
 mv ${package_tmp} ${package_json_path}
 
 git commit Cargo.toml ${plugin_toml_path} ${package_json_path} -m "chore: Bump version to $version"
+
+# Select all package dependencies that match $bump_deps_pattern and bump them to $bump_deps_version
+if [[ "$bump_deps_pattern" != '' ]]; then
+  deps=$(toml get Cargo.toml workspace.dependencies | jq -r "keys[] | select(test(\"$bump_deps_pattern\"))")
+  for dep in $deps; do
+    if [[ -n $bump_deps_version ]]; then
+      toml_set_in_place Cargo.toml "workspace.dependencies.$dep.version" "$bump_deps_version"
+    fi
+
+    if [[ -n $bump_deps_branch ]]; then
+      toml_set_in_place Cargo.toml "workspace.dependencies.$dep.branch" "$bump_deps_branch"
+    fi
+  done
+
+  package_metadata_old=$(toml get zenoh-plugin-remote-api/Cargo.toml package.metadata.deb.depends)
+  package_metadata_new=$(sed "s/.*/zenohd (=$bump_deps_version)/" <<< $package_metadata_old)
+  toml_set_in_place ${plugin_toml_path} "package.metadata.deb.depends" "$package_metadata_new"
+  
+  # Update lockfile
+  cargo check
+
+  if [[ -n $bump_deps_version || -n $bump_deps_branch ]]; then
+    git commit Cargo.toml Cargo.lock -m "chore: Bump $bump_deps_pattern version to $bump_deps_version"
+  else
+    echo "warn: no changes have been made to any workspace.dependencies matching $bump_deps_pattern"
+  fi
+fi
 
 if [[ ${live_run} ]]; then
   git tag --force "$version" -m "v$version"
