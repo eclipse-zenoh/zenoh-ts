5a31b35416ea7e3b339faae6df5800ea8c186fb8
Charles Schleich <schleich.cdaniel@gmail.com>
2024-12-27 11:07:31 +0100

    Feat/session info (#60)
    * Added ZenohId Type, TS, added message inteface to Rust plugin
    
    * Add SessionInfo Interface File, WIP add example
    
    * Export SessnionInfo, ZenohId Types, finish implementing translation into SessionInfo List of ZenohId
    
    * fmt

---
+++ zenoh-ts/examples/deno/src/z_info.ts
@@ -0,0 +1,44 @@
+//
+// Copyright (c) 2024 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+import {
+  SessionInfo, Config, Session
+} from "@eclipse-zenoh/zenoh-ts";
+
+export async function main() {
+  console.log!("Opening session...");
+  const session = await Session.open(new Config("ws/127.0.0.1:10000"));
+
+  console.log!("Get Info...");
+  let info: SessionInfo = await session.info();
+
+  console.log!("zid: {}", info.zid());
+
+  console.log!(
+    "routers zid: {:?}",
+    info.routers_zid()
+  );
+
+  console.log!(
+    "peers zid: {:?}",
+    info.peers_zid()
+  );
+
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main()
