d94ee7869a1749423c45ece3c54ef17091f76720
Michael Ilyin <milyin@gmail.com>
2025-04-03 16:48:24 +0200

    added kill zenohd by ^C

--- zenoh-ts/tests/scripts/start.sh
+++ zenoh-ts/tests/scripts/start.sh
@@ -16,19 +16,24 @@ if [ "$1" = "" ]; then
 else
   if [ "$2" = "DAEMON" ]; then
     cd "$SCRIPTDIR/../../.."
+    cargo build --release
     cargo bin -i zenohd
-    # search for zenohd in the .bin directory
     ZENOHD=$(find ./.bin -name "zenohd" -type f | head -n 1)
     if [ -z "$ZENOHD" ]; then
       echo "zenohd not found in .bin directory"
       exit 1
     fi
+    pwd
+    echo "\"$ZENOHD\" --config EXAMPLE_CONFIG.json5"
     "$ZENOHD" --config EXAMPLE_CONFIG.json5 &
 
     ZPID=$!
     echo "zenohd started with PID $ZPID"
     sleep 1
     cd "$SCRIPTDIR/.."
+
+    # Trap SIGINT to ensure zenohd is killed on ^C
+    trap "echo 'Stopping zenohd with PID $ZPID'; kill $ZPID; exit 1" SIGINT
   fi
 
   EXIT_CODE=0
