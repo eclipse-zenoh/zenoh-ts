c7ae415750abd5b7d4086eef60d7090660355f03
Denis Biryukov <denis.biryukov@zettascale.tech>
2025-05-16 16:32:03 +0200

    remove unnecessary changes

--- zenoh-ts/src/key_expr/zenoh_keyexpr_wrapper.js
+++ zenoh-ts/src/key_expr/zenoh_keyexpr_wrapper.js
@@ -2,4 +2,4 @@ import * as wasm from "./zenoh_keyexpr_wrapper_bg.wasm";
 export * from "./zenoh_keyexpr_wrapper_bg.js";
 import { __wbg_set_wasm } from "./zenoh_keyexpr_wrapper_bg.js";
 __wbg_set_wasm(wasm);
-//wasm.__wbindgen_start();
+wasm.__wbindgen_start();
