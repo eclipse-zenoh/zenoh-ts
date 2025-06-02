169fc6a83d2991840dfb9b8cc8c0ecaea90da49e
Michael Ilyin <milyin@gmail.com>
2025-06-02 12:07:33 +0200

    import zenoh-ts in vue added

--- zenoh-ts/examples/browser/nuxt/app.vue
+++ zenoh-ts/examples/browser/nuxt/app.vue
@@ -1,6 +1,9 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
+<script setup lang="ts">
+import { Config } from '@eclipse-zenoh/zenoh-ts';
+onMounted(() => {
+  document.body.innerHTML = '';
+  const helloDiv = document.createElement('div');
+  helloDiv.textContent = 'Hello World';
+  document.body.appendChild(helloDiv);
+})
+</script>
