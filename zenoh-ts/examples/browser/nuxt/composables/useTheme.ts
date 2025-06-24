8872dc3c6a143510251bb2b9cc14512c6d06a94b
Michael Ilyin <milyin@gmail.com>
2025-06-24 15:07:08 +0200

    splitted colors from boxed theme

--- zenoh-ts/examples/browser/nuxt/composables/useTheme.ts
+++ zenoh-ts/examples/browser/nuxt/composables/useTheme.ts
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 
-export type ThemeName = 'none' | 'boxed'
+export type ThemeName = 'none' | 'boxed' | 'boxed-colored'
 
 interface Theme {
   name: string
@@ -18,6 +18,11 @@ const themes: Record<ThemeName, Theme> = {
     name: 'boxed',
     displayName: 'Boxed',
     cssFile: '/themes/boxed.css'
+  },
+  'boxed-colored': {
+    name: 'boxed-colored',
+    displayName: 'Boxed Colored',
+    cssFile: '/themes/boxed-colored.css'
   }
 }
 
